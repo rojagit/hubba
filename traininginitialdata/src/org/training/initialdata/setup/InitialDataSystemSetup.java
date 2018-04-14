b5da22109afcd6ee25f8ceef1b812598a0abbb18
roja git <rojagit@github.com>
2018-04-14 11:05:30 -0400

    develop take 1

--- traininginitialdata/src/org/training/initialdata/setup/InitialDataSystemSetup.java
+++ traininginitialdata/src/org/training/initialdata/setup/InitialDataSystemSetup.java
@@ -13,6 +13,9 @@ package org.training.initialdata.setup;
 import de.hybris.platform.commerceservices.dataimport.impl.CoreDataImportService;
 import de.hybris.platform.commerceservices.dataimport.impl.SampleDataImportService;
 import de.hybris.platform.commerceservices.setup.AbstractSystemSetup;
+import de.hybris.platform.commerceservices.setup.data.ImportData;
+import de.hybris.platform.commerceservices.setup.events.CoreDataImportedEvent;
+import de.hybris.platform.commerceservices.setup.events.SampleDataImportedEvent;
 import de.hybris.platform.core.initialization.SystemSetup;
 import de.hybris.platform.core.initialization.SystemSetup.Process;
 import de.hybris.platform.core.initialization.SystemSetup.Type;
@@ -22,6 +25,7 @@ import de.hybris.platform.core.initialization.SystemSetupParameterMethod;
 import org.training.initialdata.constants.TrainingInitialDataConstants;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.apache.log4j.Logger;
@@ -40,6 +44,14 @@ public class InitialDataSystemSetup extends AbstractSystemSetup
 	private static final String IMPORT_CORE_DATA = "importCoreData";
 	private static final String IMPORT_SAMPLE_DATA = "importSampleData";
 	private static final String ACTIVATE_SOLR_CRON_JOBS = "activateSolrCronJobs";
+	private static final String IMPORT_CORE_PRODUCT_DATA = "importCoreProductData";
+
+	// Rack Room Shoes
+	private static final String my_PRODUCT_CATALOG_NAME = "my";
+	private static final String my_CONTENT_CATALOG_NAME = "trainingstore";
+	private static final String my_STORE_NAME = "trainingstore";
+
+	// Off Broadway Shoes
 
 	private CoreDataImportService coreDataImportService;
 	private SampleDataImportService sampleDataImportService;
@@ -74,6 +86,7 @@ public class InitialDataSystemSetup extends AbstractSystemSetup
 		// Add Essential Data here as you require
 	}
 
+	
 	/**
 	 * Implement this method to create data that is used in your project. This method will be called during the system
 	 * initialization. <br>
@@ -104,6 +117,56 @@ public class InitialDataSystemSetup extends AbstractSystemSetup
 		/*
 		 * Add import data for each site you have configured
 		 */
+		final boolean coreData = getBooleanSystemSetupParameter(context, IMPORT_CORE_DATA);
+		final boolean coreProductData = getBooleanSystemSetupParameter(context, IMPORT_CORE_PRODUCT_DATA);
+		final boolean sampleData = getBooleanSystemSetupParameter(context, IMPORT_SAMPLE_DATA);
+
+		if (coreData) {
+			final List<ImportData> importData = new ArrayList<>();
+
+			// Rack Room Shoes
+			final ImportData my_coreImportData = new ImportData();
+			my_coreImportData.setProductCatalogName(my_PRODUCT_CATALOG_NAME);
+			my_coreImportData.setContentCatalogNames(Arrays.asList(my_CONTENT_CATALOG_NAME));
+			my_coreImportData.setStoreNames(Arrays.asList(my_STORE_NAME));
+			importData.add(my_coreImportData);
+
+
+			// Process importData
+			getCoreDataImportService().execute(this, context, importData);
+			getEventService().publishEvent(new CoreDataImportedEvent(context, importData));
+		}
+
+		if (coreProductData) {
+			// Core stuff here
+
+		}
+
+		if (sampleData) {
+			final List<ImportData> sampleImportData = new ArrayList<>();
+
+			// Rack Room Shoes
+			final ImportData my_sampleImportData = new ImportData();
+			my_sampleImportData.setProductCatalogName(my_PRODUCT_CATALOG_NAME);
+			my_sampleImportData.setContentCatalogNames(Arrays.asList(my_CONTENT_CATALOG_NAME));
+			my_sampleImportData.setStoreNames(Arrays.asList(my_STORE_NAME));
+			sampleImportData.add(my_sampleImportData);
+
+
+			// Process importData
+			getSampleDataImportService().execute(this, context, sampleImportData);
+			getEventService().publishEvent(new SampleDataImportedEvent(context, sampleImportData));
+
+//			// Load Additional Access Rights
+//			importAccessRights(context.getExtensionName());
+//
+//			// Load Product Workflows - Rack Room
+//			importWorkflows(context.getExtensionName(), my_PRODUCT_CATALOG_NAME);
+//
+//			// Load Product Workflows - Off Broadway
+//			importWorkflows(context.getExtensionName(), OBS_PRODUCT_CATALOG_NAME);
+		}
+		
 	}
 
 	public CoreDataImportService getCoreDataImportService()
